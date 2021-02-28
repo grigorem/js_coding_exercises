dd941fd028b4e475e29d17132aec2134b2a451e3
Marius <mariusgrygore@gmail.com>
2021-02-28 22:27:30 +0000

    Implemented "reverseWord" function

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -48,7 +48,9 @@ function getMiddleCharacter(str) {
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+
+  // split all characters, revers the order in array and join them back (ASCII only)
+  return word.split("").reverse().join("");
 }
 
 function reverseAllWords(words) {
