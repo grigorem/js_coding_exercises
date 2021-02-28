6d891259efb191c0474cbe956e8f8f53e1669a6a
Marius <mariusgrygore@gmail.com>
2021-02-28 21:51:43 +0000

    Implemented "generateInitials" function

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -8,7 +8,9 @@ function capitalize(word) {
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  // Add your code here!
+
+  // isolate first character from each word, separate them with "." and return
+  return firstName[0] + "." + lastName[0];
 }
 
 function addVAT(originalPrice, vatRate) {
