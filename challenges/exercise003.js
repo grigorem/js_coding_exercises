468e43ba60f4ebb276797b33a2f6cca854769fdc
Marius <mariusgrygore@gmail.com>
2021-03-01 00:47:10 +0000

    Implemented "camelCaseWords" function

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -10,7 +10,16 @@ function getSquares(nums) {
 
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Your code here!
+
+  // save the first word as it is
+  let result = words[0];
+
+  // iterate over all words, starting with the 2nd, make the first letter as capital letter and append it to the result
+  for (let i = 1; i < words.length; i++) {
+    result += words[i][0].toUpperCase() + words[i].substring(1);
+  }
+
+  return result;
 }
 
 function getTotalSubjects(people) {
