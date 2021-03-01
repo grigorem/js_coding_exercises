84855f16929e1846ba2d66fdb6c7efa17651c0b0
Marius <mariusgrygore@gmail.com>
2021-03-01 00:20:54 +0000

    Implemented "countSheep" function

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -21,7 +21,9 @@ function getBusNumbers(people) {
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+
+  // user filter function and lambda functions to count the sheep
+  return arr.filter((elem) => elem === "sheep").length;
 }
 
 function hasMPostCode(person) {
